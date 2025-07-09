5338ce065e5ba4c7d90a0ddccb22d50f369a5e55
suparna1may <suparna1may@gmail.com>
2025-07-09 18:56:27 +0530

    third commit

--- vite-project/src/compontnts/Arrival/Arrival.jsx
+++ vite-project/src/compontnts/Arrival/Arrival.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
 import ArrivalImg1 from '../../assets/images/product/arrival-product1.jpg'
 import ArrivalImg2 from '../../assets/images/product/arrival-product2.jpg'
 import ArrivalImg3 from '../../assets/images/product/arrival-product3.jpg'
 
 function Arrival() {
+  const items = useSelector((state) => state.allCart.items);
+  const dispatch = useDispatch();
   return (
     <>
        <section className="arrival_sec">
@@ -22,53 +25,26 @@ function Arrival() {
                     <Link to="/Shop" className="shop_btn">Shop Now <i className="fa fa-bag-shopping"></i></Link>
                 </div>
 
-                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-lg-12">
-                <Link to="/">
+
+                {items.slice(-3).map((item) => (
+                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-lg-12">             
+              
                     <div className="arrival_pro">
-                      <img src={ArrivalImg1} className="img-fluid"/>
+                      <img src={item.src} className="img-fluid"/>
                       <div className="arrival_pro_wrap d-flex justify-content-between">
                       <div style={{width:'90%'}}>
-                          <h3>Men's Formal Trousers</h3>
-                          <h4>$120.00</h4>
+                          <h3>{item.name}</h3>
+                          <h4>${item.price}</h4>
                           </div>                       
-                          <Link to="/"><span><i className="fa fa-bag-shopping"></i></span></Link>
+                          <Link to={`/product/${item.id}`}><span><i className="fa fa-bag-shopping"></i></span></Link>
                        
                       </div>                    
                     </div>     
-                  </Link>                 
+                               
                 </div>
 
-                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-lg-12">
-                  <Link to="/">
-                    <div className="arrival_pro">
-                      <img src={ArrivalImg2} className="img-fluid"/>
-                      <div className="arrival_pro_wrap d-flex justify-content-between">
-                      <div style={{width:'90%'}}>
-                          <h3>Women's Kurta</h3>
-                          <h4>$90.00</h4>   
-                          </div>                    
-                          <Link to="/"><span><i className="fa fa-bag-shopping"></i></span></Link>
-                       
-                      </div>                    
-                    </div>     
-                  </Link>                 
-                </div>
+                ))}
 
-                <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-lg-12">
-                  <Link to="/">
-                    <div className="arrival_pro">
-                      <img src={ArrivalImg3} className="img-fluid"/>
-                      <div className="arrival_pro_wrap d-flex justify-content-between">
-                      <div style={{width:'90%'}}>
-                          <h3>Women's Sling Bag</h3>
-                          <h4>$65.00</h4>    
-                          </div>                   
-                          <Link to="/"><span><i className="fa fa-bag-shopping"></i></span></Link>
-                       
-                      </div>                    
-                    </div>     
-                  </Link>                 
-                </div>
 
 
               </div>
